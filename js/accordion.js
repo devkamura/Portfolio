d3eb86178602ad63c0739549694e895208be5cd0
devkam <devkamura@gmail.com>
2024-04-10 22:47:50 +0900

    テーブル角丸実装完了

--- js/accordion.js
+++ js/accordion.js
@@ -6,11 +6,17 @@ $(function(){
     $(".skills-accordion-btn-container").click(function(){
         if (accordionBtnToggle) {
             $(".material-symbols-outlined").css("transform", "scaleY(1)");
+            $(".skills-table tr:nth-child(5) td:last-child").css("border-radius", "0 0 8px 0");
+            $(".skills-table tr:nth-child(5) th, .skills-table tr:nth-child(5) td").css("border-bottom", "2px #504A4A solid");
+            $(".skills-table tr:nth-child(5) th").css("border-radius", "0 0 0 8px");
             $(".skills-table tr:gt(4)").hide();
             $(".skills-accordion-btn-container span:first-of-type()").html("もっと見る");
             accordionBtnToggle = false;
         } else {
             $(".material-symbols-outlined").css("transform", "scaleY(-1)");
+            $(".skills-table tr:nth-child(5) td:last-child").css("border-radius", "0");
+            $(".skills-table tr:nth-child(5) th, .skills-table tr:nth-child(5) td").css("border-bottom", "1px #504A4A solid");
+            $(".skills-table tr:nth-child(5) th").css("border-radius", 0);
             $(".skills-table tr:gt(4)").show();
             $(".skills-accordion-btn-container span:first-of-type()").html("閉じる");
             accordionBtnToggle = true;
